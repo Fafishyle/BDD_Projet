621488006c3065d976d651b99e9700a6d0659d3a
Fafishyle <finaritralft@gmail.com>
2023-10-05 12:03:27 +0200

    Update BDD_projet.sql

--- BDD_projet.sql
+++ BDD_projet.sql
@@ -1,1 +1,8 @@
-select * from emp;
+create or replace function unify () returns void as $$
+begin
+	/*Détruit la table C_ALL si elle existe */
+	DROP TABLE IF exists C_ALL;
+end
+$$language plpgsql;
+
+select unify();
