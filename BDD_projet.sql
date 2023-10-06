7b529035b12caf92e36c2d0a6f7bd70ac019af7f
Fafishyle <finaritralft@gmail.com>
2023-10-06 14:37:57 +0200

    3ieme point du sujet, fonction name_catalog()
    - parcourt la table META pour obtenir les noms des catalogues (C1, C2 et C3 dans notre exemple) ;
    - Créer la fonction name_catalog() qui retourne le nom des catalogue

--- BDD_projet.sql
+++ BDD_projet.sql
@@ -1,10 +1,40 @@
-/*Fonction unify_catalog qui ne prend rien en paramètre
- * return */
-create or replace function unify_catalog () returns void as $$
+/*Parcourt la table META pour obtenir les noms des catalogues*/
+create or replace function name_catalog() returns setof meta.table_name%type as $$
+declare
+	nuplet record;
+begin 
+	for nuplet in SELECT * FROM meta loop
+		return next nuplet.table_name;
+	end loop;
+return;
+end
+
+$$ language plpgsql;
+/* Fonction unify_catalog 
+ * ne prend rien en paramètre
+ * ne retourne rien */
+create or replace function unify_catalog() returns void as $$
 begin
 	/*Détruit la table C_ALL si elle existe */
 	DROP TABLE IF exists C_ALL;
+
+	/*Crée une table C_ALL*/
+	CREATE TABLE C_ALL
+	(
+    	pid NUMERIC(5)PRIMARY KEY,
+    	pname VARCHAR(50),
+    	pprice NUMERIC(8,2)
+	);
+	
+	/**/
+	SELECT name_catalog();
+
+	/*Récupère dans le schéma de chaque catalogue 
+	 * les noms des attributs qui contiennent 
+	 * name et price,
+	 * respectivement ;*/
+	
 end
 $$language plpgsql;
 
-select unify();
+select unify_catalog();
