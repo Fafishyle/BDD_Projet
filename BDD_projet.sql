51bf5f81d429cb37974040910b1de8c30cc63f00
Fafishyle <finaritralft@gmail.com>
2023-10-15 15:04:43 +0200

    Ajustement de requête
    requete n'a pas besoin de pid vu qu'on le crée dynamiquement avec key_pid.
    J'ai renommé indicePid en key_pid car tous les noms de variables sont en Anglais! et ont un UNDERSCORE

--- BDD_projet.sql
+++ BDD_projet.sql
@@ -5,11 +5,11 @@
  * ne retourne rien
  */
 create or replace function unify_catalog() returns void as $$
-declare
+DECLARE
 	/* cat : nom de catalogue
 	 * att_name : attribut d'un catalogue contennant name
 	 * att_price : attribut d'un catalogue contennant price
-	 * indicePid : clef primaire de la table C_ALL
+	 * key_pid : clef primaire de la table C_ALL
 	 * cursDyn : curseur dynamique
 	 * requete : permet de construire une requete qui cherche à partir du pid, du nom de l'attribut name et du nom de l'attribut pprice
 	 * res : enregistrement resultat (pid, pname, pprice) à charger dans la table
@@ -18,7 +18,7 @@ declare
 	cat VARCHAR;
 	att_name VARCHAR;
 	att_price VARCHAR;
-	indicePid int := 1;
+	key_pid INT := 1;
 	cursDyn REFCURSOR;
 	requete VARCHAR; 
 	res record;
@@ -35,7 +35,7 @@ BEGIN
     	pprice NUMERIC(8,2)
 	);
 	-- Parcourt la table META
-	for cat in SELECT table_name FROM meta loop
+	FOR cat IN SELECT table_name FROM meta LOOP
 		cat := LOWER(cat);
 		raise notice 'Pour le catalogue : %',cat;
 		/* Récupère dans le schéma de chaque catalogue
@@ -44,7 +44,7 @@ BEGIN
 		SELECT column_name into att_name
 		FROM information_schema.columns
 		WHERE table_name = cat
-		and 
+		AND 
 		column_name ILIKE '%name%';
 		raise notice '- d attribut name : %',att_name;
 		/* Récupère dans le schéma de chaque catalogue
@@ -61,7 +61,6 @@ BEGIN
          * Construction de la requête du curseur dynamique
          */
         requete := 'SELECT ' 
-        			|| indicePid || 'AS pid, '
        				|| att_name  || ' AS pname, ' 
        				|| att_price || ' AS pprice 
 					FROM ' || cat;
@@ -72,8 +71,7 @@ BEGIN
        	FETCH cursDyn into res;
         LOOP
             EXIT WHEN NOT FOUND;
-            res.pid := indicePid;
-            raise notice 'L enregistrement (pid, pname , pprice) est : %', res;
+            raise notice 'L enregistrement (pid, pname , pprice) est : (%,%,%)',key_pid, res.pname,res.pprice;
            /* Verifie dans la table META 
             * si des transformations sont à appliquer  aux données
             * avant de les inserer dans la table C_ALL
@@ -94,14 +92,14 @@ BEGIN
            		IF code LIKE '%CUR%' THEN
            			res.pprice := res.pprice / 1.05;
            		END IF;
-           		raise notice '	et en appliquant les transformations : % .', res;
+           		raise notice '	et en appliquant les transformations : (%,%,%)',key_pid, res.pname,res.pprice;
 			END IF;
 			/* Insère les données dans C_ALL
 			 * après avoir effectué toutes les modifications nécessaires
 			 */
-            INSERT INTO C_ALL (pid, pname, pprice) VALUES (res.pid, res.pname, res.pprice);
+            INSERT INTO C_ALL (pid, pname, pprice) VALUES (key_pid, res.pname, res.pprice);
             FETCH cursDyn into res;
-           	indicePid := indicePid + 1;
+           	key_pid := key_pid + 1;
         END LOOP;
         CLOSE cursDyn;
       raise notice E'\n';
